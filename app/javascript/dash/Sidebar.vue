1f6045dcacd1468105a46de9aa8d6d1d56664208
Ousmane Kebe <kebe.4@osu.edu>
2017-08-07 13:02:14 -0400

    style fix

--- app/javascript/dash/Sidebar.vue
+++ app/javascript/dash/Sidebar.vue
@@ -2,13 +2,13 @@
     <div id="sidebar" class="text-center">
         <ul id="slide-out" class="side-nav">
             <li><div><img src="/logo_v2.jpg" class="img-circle" ></div> </li>
-            <li><a href="#"><i class="material-icons">person</i>Driver: {{user.first_name + ' ' + user.last_name}}</a></li>
+            <li><a href="#" style="font-size:1.3em;"><i class="material-icons">person</i>Driver: {{user.first_name + ' ' + user.last_name}}</a></li>
             <li><a href="#" @click.prevent="logout"><i class="material-icons">power_settings_new</i> <h5>Logout</h5></a></li>
             <li><div class="divider"></div></li>
             <li><router-link :to="'/dashboard'" class="waves-effect">
                     <i class="material-icons">dashboard</i> <h5>Dashboard</h5>
                 </router-link></li>
-            <li><a class="subheader">My Routes</a></li>
+            <li><a class="subheader"><h5>My Routes</h5></a></li>
             <ul>
               <li v-for="client_route in user.routes">
               <router-link :to="'/route/'+client_route.id" class="waves-effect">
