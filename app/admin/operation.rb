2af55c53fcc4ed38c4f8e8166cb002be579b9f4d
Ousmane Kebe <kebe.4@osu.edu>
2017-08-12 02:00:13 -0400

    added repotrst

--- app/admin/operation.rb
+++ app/admin/operation.rb
@@ -11,5 +11,30 @@ permit_params :type, :time, :client_id, :driver_id, :status, :helper, :wheelchai
 #   permitted << :other if params[:action] == 'create' && current_user.admin?
 #   permitted
 # end
+	filter :route
+    filter :contract
+    filter :driver
+ 	filter :client
+    filter :time
+	filter :op_type 
+    filter :status
+    filter :helper
+    filter :wheelchair
+    
+
+  index do
+    selectable_column
+    id_column
+    column :op_type
+    column :time
+    column :client
+    column :driver
+    column :status
+    column :helper
+    column :wheelchair
+    column :route
+    column :contract
+    actions
+  end
 
 end
--- app/models/operation.rb
+++ app/models/operation.rb
@@ -1,6 +1,8 @@
 class Operation < ApplicationRecord
   belongs_to :client
   belongs_to :driver
+  has_one :route, through: :client 
+  has_one :contract, through: :client 
 
   before_create do
     self.time = DateTime.now unless self.time
